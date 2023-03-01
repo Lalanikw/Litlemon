9d2c433d9fb8986dd51345723d52a157a236f813
Lalani Werake <lalanikw@gmail.com>
2023-03-01 12:39:38 -1000

    adding index and App files after setting up the blocks

--- lemon/src/App.js
+++ lemon/src/App.js
@@ -1,25 +1,41 @@
 import './App.css';
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import Footer from "./Components/Footer";
 import Testimonials from "./Components/Testimonials";
 import Specials from "./Components/Specials";
 import About from "./Components/About";
+import Home from "./Components/Header";
 
 function App() {
   return (
-    <>
-      <main>
-        <ul>
-          <Header/>
-          <Main/>
-          <Specials/>
-          <Testimonials/>
-          <About/>
-          <Footer/>
-        </ul>
-      </main>
-    </>
+    <div class="container">
+        <nav class="Header">
+            <Header >
+                <ul class="Nav_bar_content">
+                    <li><a href={Home}> Home</a></li>
+                    <li><a href={About}> About</a></li>
+                    <li><a href={Specials}> Menu</a></li>
+                    <li><a href={Main}> Reservations</a></li>
+                    <li><a href={Specials}> Order Online</a></li>
+                    <li><a href="/Login"> Login</a></li>
+                </ul>
+            </Header>
+        </nav>
+        <main class="Main">
+            <div class="Main_section">
+                <ul>
+                    <li class="Hero"><Main />Main</li>
+                    <li class="Specials"><Specials/>Specials</li>
+                    <li class="Testimonials"><Testimonials />Testimonials</li>
+                    <li class="About"><About />About</li>
+                </ul>
+            </div>
+        </main>
+        <footer class="Footer">
+            <Footer/>
+        </footer>
+    </div>
   );
 }
 
